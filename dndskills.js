e521cf3d5035313d73b2d37b2a4168585d0d2c95
Fabian Maurer <maurerfabi@gmail.com>
2019-07-12 23:11:55 +0200

    initial commit

---
+++ dndskills.js
@@ -0,0 +1,691 @@
+let skills = [
+    {
+        name: 'Appraise',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Autohypnosis',
+        ability: 'wis',
+        synergies: ['Knowledge (Psionics)'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Balance',
+        ability: 'dex',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Bluff',
+        ability: 'cha',
+        synergies: ['Diplomacy', 'Disguise', 'Intimidate', 'Sleight of Hand'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Climb',
+        ability: 'str',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Concentration',
+        ability: 'con',
+        synergies: ['Autohypnosis'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Craft (Alchemy)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Craft (Armorsmithing)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Craft (Weaponsmithing)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Decipher Script',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Diplomacy',
+        ability: 'cha',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Disable Device',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Disguise',
+        ability: 'cha',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Escape Artist',
+        ability: 'dex',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Forgery',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Gather Information',
+        ability: 'cha',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Handle Animal',
+        ability: 'cha',
+        synergies: ['Ride'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Heal',
+        ability: 'wis',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Hide',
+        ability: 'dex',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Intimidate',
+        ability: 'cha',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Jump',
+        ability: 'str',
+        synergies: ['Tumble'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Knowledge (Arcana)',
+        ability: 'int',
+        synergies: ['Spellcraft'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Architecture)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Dungeoneering)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Geography)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (History)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Local)',
+        ability: 'int',
+        synergies: ['Gather Information'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Nature)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Nobility)',
+        ability: 'int',
+        synergies: ['Diplomacy'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Planes)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Psionics)',
+        ability: 'int',
+        synergies: ['Psicraft'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Knowledge (Religion)',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Listen',
+        ability: 'wis',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Move Silently',
+        ability: 'dex',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Open Lock',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Perform',
+        ability: 'cha',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Psicraft',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Ride',
+        ability: 'dex',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Search',
+        ability: 'int',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Sense Motive',
+        ability: 'wis',
+        synergies: ['Diplomacy'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Sleight of Hand',
+        ability: 'dex',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Spellcraft',
+        ability: 'wis',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Spot',
+        ability: 'wis',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Survival',
+        ability: 'wis',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Swim',
+        ability: 'str',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }, {
+        name: 'Tumble',
+        ability: 'dex',
+        synergies: ['Balance', 'Jump'],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Use Magic Device',
+        ability: 'cha',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Use Psionic Device',
+        ability: 'cha',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: false
+    }, {
+        name: 'Use Rope',
+        ability: 'dex',
+        synergies: [],
+        ranks: 0,
+        bonusRanks: 0,
+        crossClass: false,
+        strategy: 'set',
+        default: true
+    }
+]
+
+let lvl = 7;
+let ranksPerLevel = 11;
+let totalRanks = ranksPerLevel * (3 + lvl);
+let usedRanks = 0;
+$('#lvl').keyup(function () {
+    if (!isNaN($('#lvl')[0].value) && ($('#lvl')[0].value != '')) {
+        lvl = parseInt($('#lvl')[0].value);
+        maxranks = lvl + 3;
+        totalRanks = ranksPerLevel * (3 + lvl);
+        initTable();
+    }
+})
+
+$('#lvl').change(function () {
+    if (!isNaN($('#lvl')[0].value) && ($('#lvl')[0].value != '')) {
+        lvl = parseInt($('#lvl')[0].value);
+        maxranks = lvl + 3;
+        totalRanks = ranksPerLevel * (3 + lvl);
+        initTable();
+    }
+})
+
+$('#ranks').keyup(function () {
+    if (!isNaN($('#ranks')[0].value) && ($('#ranks')[0].value != '')) {
+        ranksPerLevel = parseInt($('#ranks')[0].value);
+        totalRanks = ranksPerLevel * (3 + lvl);
+        initTable();
+    }
+})
+
+$('#ranks').change(function () {
+    if (!isNaN($('#ranks')[0].value) && ($('#ranks')[0].value != '')) {
+        ranksPerLevel = parseInt($('#ranks')[0].value);
+        totalRanks = ranksPerLevel * (3 + lvl);
+        initTable();
+    }
+})
+let maxranks = lvl + 3;
+
+let strategies = ['set', '25%', '50%', '75%', '100%']
+
+let stringData= localStorage['dndSkillsData'];
+if(stringData){
+    let dataObj=JSON.parse(stringData);
+    skills=dataObj.skills;
+    lvl=dataObj.lvl;
+    ranksPerLevel=dataObj.ranksPerLevel;
+    maxranks = lvl + 3;
+    totalRanks = ranksPerLevel * (3 + lvl);
+}
+
+$('#save').click(function(){
+    let dataObj={
+        skills:skills,
+        lvl:lvl,
+        ranksPerLevel:ranksPerLevel
+    }
+    localStorage['dndSkillsData']=JSON.stringify(dataObj)
+})
+
+
+initTable()
+
+function initTable() {
+    console.log('refresh table')
+    checkRanks();
+    checkSynergies();
+    let $table = $('#skills');
+    $table.empty();
+    for (let skill of skills) {
+        let $row = $(document.createElement('tr'))
+        let $name = $(document.createElement('td'))
+        let $count = $(document.createElement('td'))
+        $count.css('width','24px')
+        $count.css('text-align','center')
+        $count.html('<div class="skillcount">' + ((skill.ranks == 0 && !skill.default) ? '-' : (skill.ranks + skill.bonusRanks)) + '</div>')
+        $name.html('<div class="skillname">' + skill.name + '</div>')
+        let $cross = $(document.createElement('td'))
+        let $check = $(document.createElement('input'))
+        $check.attr('type', 'checkbox')
+        if (!skill.crossClass) {
+            $check.attr('checked', 'true')
+        }
+
+        $check.change(function (e) {
+            toggleCrossclass(skill, this.checked)
+        })
+        $cross.append($check);
+        let $ranks = $(document.createElement('td'));
+        let max = skill.crossClass ? Math.floor(maxranks / 2) + skill.bonusRanks : maxranks + skill.bonusRanks;
+        for (let i = 0; i < max; i++) {
+            let $r = $(document.createElement('div'));
+            if (i < skill.bonusRanks) {
+                $r.attr('class', 'rank bonus');
+                $ranks.append($r);
+            } else if (i < skill.ranks + skill.bonusRanks) {
+                $r.attr('class', 'rank enabled');
+                $r.click(function () {
+                    if (skill.ranks + skill.bonusRanks == i + 1) {
+                        setRank(skill, 0);
+                    } else {
+                        setRank(skill, i + 1 - skill.bonusRanks);
+                    }
+                })
+                $ranks.append($r);
+            } else if (skill.crossClass && (i >= skill.ranks + skill.bonusRanks + Math.floor((totalRanks - usedRanks) / 2))) {
+                $r.attr('class', 'rank forbidden');
+                $ranks.append($r);
+            } else if (i < skill.ranks + skill.bonusRanks + totalRanks - usedRanks) {
+                $r.attr('class', 'rank disabled');
+                $r.click(function () {
+                    setRank(skill, i + 1 - skill.bonusRanks);
+                })
+                $ranks.append($r);
+            } else {
+                $r.attr('class', 'rank forbidden');
+                $ranks.append($r);
+            }
+            if (skill.synergies.length > 0 && (i == skill.bonusRanks + 4)) {
+                $r.addClass('special')
+                $r.hover(function () {
+                    let str = 'Synergy:';
+                    for (let syn of skill.synergies) str += '<br>' + syn;
+                    $('#tooltip').html(str)
+                    $('#tooltip').css('top', $r.offset().top);
+                    $('#tooltip').css('left', $r.offset().left + 20);
+                    $('#tooltip').fadeIn(200);
+                }, function () {
+                    $('#tooltip').fadeOut(200);
+                });
+            }
+        }
+        let $strats = $(document.createElement('td'));
+        $strats.css('width', '200px');
+        $strats.css('text-align', 'center');
+        let shown = false;
+        for (let strat of strategies) {
+            let $strat = $(document.createElement('div'));
+            $strat.attr('class', 'strat');
+            $strat.html(strat)
+            if (strat == skill.strategy) $strat.addClass('enabled')
+            switch (strat) {
+                case '100%':
+                    $strat.addClass('p100')
+                    break;
+                case '75%':
+                    $strat.addClass('p75')
+                    break;
+                case '50%':
+                    $strat.addClass('p50')
+                    break;
+                case '25%':
+                    $strat.addClass('p25')
+                    break;
+                default:
+                    $strat.addClass('set')
+            }
+            $strat.click(function () {
+                if (shown) {
+                    shown = false;
+                    $strats.find('enabled').removeClass('enabled');
+                    $strat.addClass('enabled');
+                    setStrategy(skill, strat);
+                } else {
+                    shown = true;
+                    $strats.find(':not(.enabled)').css('width', '40px');
+                }
+
+            })
+            $strats.append($strat);
+        }
+        $strats.find(':not(.enabled)').css('width', '0px');
+        $row.append($cross)
+        $row.append($name)
+        $row.append($count)
+        $row.append($ranks)
+        $row.append($strats)
+        $table.append($row);
+    }
+}
+
+function setStrategy(skill, strat) {
+    skill.strategy = strat;
+    initTable();
+}
+
+function checkRanks() {
+    // apply strategies
+    for (let skill of skills) {
+        let m=skill.crossClass?Math.floor(maxranks/2):maxranks;
+        switch (skill.strategy) {
+            case '100%':
+                skill.ranks = m;
+                break;
+            case '75%':
+                skill.ranks = Math.floor(m * 0.75);
+                break;
+            case '50%':
+                skill.ranks = Math.floor(m / 2);
+                break;
+            case '25%':
+                skill.ranks = Math.floor(m / 4);
+                break;
+        }
+    }
+    // count ranks
+    usedRanks = 0;
+    for (let skill of skills) {
+        // check if too many ranks
+        if(skill.ranks>maxranks) skill.ranks=maxranks;
+        usedRanks += skill.crossClass ? skill.ranks * 2 : skill.ranks;
+    }
+    $('#usedranks').html('Ranks<br>' + usedRanks + '/' + totalRanks);
+}
+
+
+function toggleCrossclass(skill, checked) {
+    skill.crossClass = !checked;
+    initTable();
+}
+
+function setRank(skill, value) {
+    skill.strategy = 'set';
+    skill.ranks = value;
+    initTable();
+}
+
+function getSkill(name) {
+    for (let skill of skills) {
+        if (skill.name == name) return skill;
+    }
+    console.log('could not find skill ' + name);
+}
+
+function checkSynergies() {
+    for (let skill of skills) {
+        skill.bonusRanks = 0;
+    }
+    for (let skill of skills) {
+        if (skill.ranks >= 5 && skill.synergies.length > 0) {
+            for (let syn of skill.synergies) {
+                getSkill(syn).bonusRanks += 2;
+            }
+        }
+    }
+}
